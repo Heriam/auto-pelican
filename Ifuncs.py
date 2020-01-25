488440dfc77b21a375e3b9f77ca61fa08e8f91c8
Justin C <haojiang@kth.se>
2020-01-25 23:09:35 +0800

    update

--- Ifuncs.py
+++ Ifuncs.py
@@ -1,10 +1,8 @@
-import subprocess
-import sys
 import Iutils as util
 
 
 # 读取配置
-ver = util.config['install_python3_version']
+py_ver = util.config['install_python3_version']
 
 
 # 更新系统
@@ -15,18 +13,23 @@ def update_packages():
 # 安装 Python3
 def install_python3():
     if not (util.python_version[0] == 3 and util.python_version[1] >= 5):
-        util.shell('yum -y groupinstall "Development Tools"')
-        util.shell('yum -y install zlib zlib-devel libffi-devel maven openssl-devel wget')
-        util.shell('wget https://www.python.org/ftp/python/%s/Python-%s.tgz' % (ver,ver))
-        util.shell('tar -zxvf Python-%s.tgz' % ver)
-        util.shell('cd Python-%s' % ver)
-        util.shell('./configure')
+        util.shell('yum -y groupinstall "Development Tools"', False)
+        util.shell('yum -y install zlib zlib-devel libffi-devel maven openssl-devel wget', False)
+        util.shell('wget https://www.python.org/ftp/python/%s/Python-%s.tgz' % (py_ver,py_ver))
+        util.shell('tar -zxvf Python-%s.tgz' % py_ver)
+        util.shell('cd Python-%s' % py_ver)
+        util.shell('./configure prefix=/usr/local/python3')
         util.shell('make')
         util.shell('make install')
-    if not util.shell('pip3 -V', False):
-
-
-
+        util.shell('cd /usr/bin')
+        util.shell('mv python python2', False)
+        util.shell('mv pip pip2', False)
+        util.shell('ln -s /usr/local/python3/bin/python3 /usr/bin/python', False)
+        util.shell('ln -s /usr/local/python3/bin/pip3 /usr/bin/pip', False)
+        util.shell('sed -i "s/#!\/usr\/bin\/python/#!\/usr\/bin\/python2/g" /usr/bin/yum', False)
+        util.shell('sed -i "s/#!\/usr\/bin\/python/#!\/usr\/bin\/python2/g" /usr/libexec/urlgrabber-ext-down', False)
+        util.shell('python -V', False)
+        util.shell('pip -V', False)
 
 
 # 安装 Pelican
