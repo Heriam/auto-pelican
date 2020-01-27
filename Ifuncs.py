53e1acdf18641337ce8c85f7ae309cd12272f879
Justin C <haojiang@kth.se>
2020-01-27 16:12:34 +0800

    update

--- Ifuncs.py
+++ Ifuncs.py
@@ -44,5 +44,4 @@ def _2_install_pelican():
     util.shell('pip install pelican Markdown')
     util.shell('mkdir blog')
     p = subprocess.Popen('cd blog && pelican-quickstart', shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-    print(p.stdout.readline())
-    print(p.stdout.readline())
+    print(p.stdout.readlines())
