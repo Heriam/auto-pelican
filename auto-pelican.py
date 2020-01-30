5b27209218f0b2a07810e0fd23e4c7c3088ef82e
Justin C <haojiang@kth.se>
2020-01-30 22:39:45 +0800

    update

--- auto-pelican.py
+++ auto-pelican.py
@@ -13,28 +13,49 @@ menu = inspect.getmembers(func, predicate=inspect.isfunction)
 
 # 打印选项
 def print_options():
-    print('\n------------ supported options ------------\n')
+    print('\n———————————— supported options ————————————\n')
     for i in range(len(menu)):
         print('[%s] %s' % (i, menu[i][0]))
-    print('\n-------------------------------------------\n')
+    print('\n———————————————————————————————————————————\n')
+
+
+# 运行入口
+def run():
+    if len(args) == 1:
+        while True:
+            select_and_run_opt(_input=True)
+    elif len(args) == 2:
+        select_and_run_opt(_args=args[1])
+    else:
+        print('Invalid arguments. Only 1 argument is supported at most. Please try again and rerun the code as below:')
+        print('python auto-pelican.py [option-number]')
+        print_options()
 
 
 # 选择并允许选项
-def select_and_run_opt():
+def select_and_run_opt(_args=None, _input=False):
+    # 初始化
     print_options()
-    try:
-        opt = input('Select option:')
-        if (type(opt) == int or opt.isdigit()) and (int(opt) < len(menu) and int(opt) >= 0):
-            menu[int(opt)][1]()
-        else:
+    opt = None
+    # 入参解析
+    if _input and not _args:
+        try:
+            opt = input('Select option:')
+        except KeyboardInterrupt as err:
+            util.info('KeyboardInterrupt Exiting.')
+            sys.exit(0)
+        except (SyntaxError, NameError) as err:
             util.info('Invalid input. Please retype the number of intended option.')
-    except KeyboardInterrupt as err:
-        util.info('KeyboardInterrupt Exiting.')
-        sys.exit(0)
-    except (SyntaxError, NameError) as err:
+    elif _args and not _input:
+        opt = _args
+    else:
+        SyntaxError('Operations must and can only be selected by either interactive input or initial arguments.')
+    # 执行操作
+    if (type(opt) == int or opt.isdigit()) and (len(menu) > int(opt) >= 0):
+        menu[int(opt)][1]()
+    else:
         util.info('Invalid input. Please retype the number of intended option.')
 
 
 if __name__ == '__main__':
-    while True:
-        select_and_run_opt()
+    run()
